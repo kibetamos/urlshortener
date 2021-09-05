c26dc0118376027091fb42218c16629287509db1
kibet kilel <amos.rotich@strathmore.edu>
2021-09-05 22:10:03 +0300

    update on functions

--- shortner/views.py
+++ shortner/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
