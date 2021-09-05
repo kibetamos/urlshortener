1ec74d60af70439878a8e92f178f0a85b32a351a
kibet kilel <amos.rotich@strathmore.edu>
2021-09-05 20:12:02 +0300

    models and admin page update

--- shortner/urls.py
+++ shortner/urls.py
@@ -1,8 +1,8 @@
-from typing import Pattern
-from django.conf.urls import url
 from django.urls import path
 from . import views
 
-urlPattern = [
-    path('', views.index , name='index')
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('create', views.create, name='create'),
+    # path('<str:pk>', views.go, name='go')
 ]
